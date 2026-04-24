399c5a59b20de5b25dd9909b3f5ba6193a04f79d
Isac Lehmkuhl dos Santos <isacldossan@gmail.com>
2026-04-24 19:57:22 -0300

    fix: status http on the request corrected

--- src/main/java/sm/Supermercado.java
+++ src/main/java/sm/Supermercado.java
@@ -21,7 +21,7 @@ public class Supermercado {
 
     public Supermercado(String url)  {
         cliente = HttpClient.newHttpClient();
-        this.url = url+ "/api/catalog_system/pub/products/search/";
+        this.url = url + "/api/catalog_system/pub/products/search/";
     }
 
     public ListaSequencial<Produto> busca(String produto) throws URISyntaxException {
@@ -35,7 +35,8 @@ public class Supermercado {
         ListaSequencial<Produto> r = new ListaSequencial<>();
         try {
             HttpResponse<String> response = cliente.send(req, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() == 200) {
+            int statusCode = response.statusCode();
+            if (statusCode == 200 || statusCode == 206) {
                 var headers = response.headers().map();
                 boolean isJson = headers.get("content-type").stream().anyMatch(x -> x.startsWith("application/json"));
                 if (isJson) {
