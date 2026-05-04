a11b9dd1318ecb8486189f967df825406266d389
Isac Lehmkuhl dos Santos <isacldossan@gmail.com>
2026-05-03 21:24:40 -0300

    feat: initial loop to capture user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,33 +2,51 @@ import esd.ListaSequencial;
 import sm.Bistek;
 import sm.Fort;
 import sm.Giassi;
-import sm.Produto;
+
+import java.util.Scanner;
 
 public class Main {
-    static void main() {
+    static Giassi sm = new Giassi();
+    static Fort fort = new Fort();
+    static Bistek bistek = new Bistek();
 
-        // cria um acessador para o Giassi
-        Giassi sm = new Giassi();
-        Fort fort = new Fort();
-        Bistek bistek = new Bistek();
+    static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        ListaSequencial<String> listaNomeProdutos = new ListaSequencial<>();
+        boolean continuar = true;
 
+        while (continuar) {
+            IO.print("> ");
+            String cmd = inp.nextLine();
+            cmd = cmd.strip();
+            switch (cmd) {
+                case "" -> {
+                }
+                case "sair" -> { // sair sem executar nada e zerando os produtos
+                    continuar = false;
+                    listaNomeProdutos.limpa();
+                }
+                case "?" -> { // Retira último produto inserido
+                    if (!listaNomeProdutos.esta_vazia()) {
+                        IO.println(listaNomeProdutos.remove_ultimo());
+                    }
+                }
+                case "produtos" -> { // lista todos os produtos
+                    listaNomeProdutos.listarElementos();
+                }
+                case "calcular" -> { // calcula o mercado com menor preço de produtos
+                    IO.println("Aqui vai ser chamado o metodo");
+                    continuar = false;
+                }
+                default -> {
+                    listaNomeProdutos.adiciona(cmd);
+                }
+            }
+        }
         // procura todos produtos cujo nome contenha "tapioca"
-        ListaSequencial<Produto> produtos = sm.busca("leite");
-        ListaSequencial<Produto> produtosFort = fort.busca("leite");
-        ListaSequencial<Produto> produtosBistek = bistek.busca("leite");
+//        ListaSequencial<Produto> produtos = sm.busca("leite");
+//        ListaSequencial<Produto> produtosFort = fort.busca("leite");
+//        ListaSequencial<Produto> produtosBistek = bistek.busca("leite");
 
-        // Mostra cada um dos produtos encontrados
-        for (int pos=0; pos < produtos.comprimento(); pos++) {
-            IO.println(produtos.obtem(pos));
-        }
-
-        // Mostra cada um dos produtos encontrados
-        for (int pos=0; pos < produtosFort.comprimento(); pos++) {
-            IO.println(produtosFort.obtem(pos));
-        }
-        // Mostra cada um dos produtos encontrados
-        for (int pos=0; pos < produtosBistek.comprimento(); pos++) {
-            IO.println(produtosBistek.obtem(pos));
-        }
     }
 }
